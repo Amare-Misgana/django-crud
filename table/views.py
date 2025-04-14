cc00abd1763d8c3dfd7b7137a2f7c0a119ceb2dd
Amare Misgana <codeamare@example.com>
2025-04-14 23:06:09 +0300

    with edit

--- table/views.py
+++ table/views.py
@@ -12,7 +12,16 @@ def create_table(request):
 def pre_edit_table(request, id):
     student = MarkList.objects.get(id=id)
     return render(request, "table/edit_table.html", {"student": student})
-    
+def edit_table(request, id):
+    student = MarkList.objects.get(id=id)
+    if request.method == "POST":
+        student_name = request.POST.get("student")
+        mark = request.POST.get("mark")
+        student.student_name = student_name
+        student.mark = mark
+        student.save()
+        return redirect("table_url")
+
 
 
 def table(request):
